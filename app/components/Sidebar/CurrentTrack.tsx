517215a8f6e6c4f46cc79b57226f6c41d3517271
Jake Taylor <me@jaketaylor.co>
2022-02-10 22:11:14 +0000

    Make sure music icon only shows when listening, also added a lil snazz

---
+++ app/components/Sidebar/CurrentTrack.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'remix';
+import type { Track } from '~/lib/loadTrack';
+
+type Props = {
+  /**
+   * Controls if the currently playing dialog has been expanded on _mobile_
+   * On desktop, We always show the dialog, in which case this is ignored, using some tailwind.
+   */
+  open: boolean;
+  track: Track;
+};
+
+export const CurrentTrack: React.FC<Props> = ({ track, open }) => {
+  return (
+    <div
+      className={`absolute bottom-full md:bottom-0 w-full ${
+        open ? 'block' : 'hidden md:block'
+      }`}
+    >
+      <Link
+        className="group block w-full bg-gray-700 hover:bg-gray-600 transition-colors p-2 text-right md:text-left"
+        to="/music"
+      >
+        <p className="whitespace-nowrap truncate text-xs font-semibold text-gray-300 text-opacity-50 mb-2 uppercase tracking-wider">
+          Listening Now
+        </p>
+
+        <div className="flex flex-row-reverse md:flex-row items-center justify-start">
+          <img
+            src={track.album_art}
+            alt={track.album_name}
+            className="w-12 h-12 ml-2 md:ml-0 mr-0 md:mr-2"
+          />
+
+          <div className="overflow-hidden">
+            <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-90">
+              {track.track_name}
+            </p>
+            <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-75">
+              {track.artist_name}
+            </p>
+            <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-75">
+              {track.album_name}
+            </p>
+          </div>
+        </div>
+
+        <div className="md:hidden absolute top-full right-6 border-12 border-transparent border-t-gray-700 group-hover:border-t-gray-600 transition-colors"></div>
+      </Link>
+    </div>
+  );
+};
--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { HomeIcon } from './icons/Home';
-import { CollectionIcon } from './icons/Collection';
-import { ChatIcon } from './icons/Chat';
+import { HomeIcon } from '../icons/Home';
+import { CollectionIcon } from '../icons/Collection';
+import { ChatIcon } from '../icons/Chat';
 import { Link, NavLink, useLocation } from 'remix';
 import { Track } from '~/lib/loadTrack';
-import { MusicIcon } from './icons/Music';
+import { MusicIcon } from '../icons/Music';
+import { CurrentTrack } from './CurrentTrack';
 
 type Props = {
   track: Track | null;
@@ -80,55 +81,20 @@ export const Sidebar: React.FC<Props> = ({ track }) => {
             </NavLink>
           </li>
 
-          <li className="md:hidden absolute right-0 top-1/2 transform -translate-y-1/2">
-            <button
-              className="block bg-indigo-500 bg-opacity-20 text-indigo-500 rounded-full p-2"
-              onClick={() => setTrackOpen((p) => !p)}
-            >
-              <MusicIcon className="block h-5 w-5 opacity-75 -mr-[2px] -mb-[2px]" />
-            </button>
-          </li>
+          {track ? (
+            <li className="md:hidden absolute right-0 top-1/2 transform -translate-y-1/2">
+              <button
+                className="block bg-indigo-500 bg-opacity-20 text-indigo-500 rounded-full p-2"
+                onClick={() => setTrackOpen((p) => !p)}
+              >
+                <MusicIcon className="block h-5 w-5 opacity-75 -mr-[2px] -mb-[2px] motion-safe:animate-pulse" />
+              </button>
+            </li>
+          ) : null}
         </ul>
       </nav>
 
-      {track ? (
-        <div
-          className={`absolute bottom-full md:bottom-0 w-full ${
-            trackOpen ? 'block' : 'hidden md:block'
-          }`}
-        >
-          <Link
-            className="group block w-full bg-gray-700 hover:bg-gray-600 transition-colors p-2 text-right md:text-left"
-            to="/music"
-          >
-            <p className="whitespace-nowrap truncate text-xs font-semibold text-gray-300 text-opacity-50 mb-2 uppercase tracking-wider">
-              Listening Now
-            </p>
-
-            <div className="flex flex-row-reverse md:flex-row items-center justify-start">
-              <img
-                src={track.album_art}
-                alt={track.album_name}
-                className="w-12 h-12 ml-2 md:ml-0 mr-0 md:mr-2"
-              />
-
-              <div className="overflow-hidden">
-                <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-90">
-                  {track.track_name}
-                </p>
-                <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-75">
-                  {track.artist_name}
-                </p>
-                <p className="whitespace-nowrap truncate text-xs text-gray-300 text-opacity-75">
-                  {track.album_name}
-                </p>
-              </div>
-            </div>
-
-            <div className="md:hidden absolute top-full right-6 border-12 border-transparent border-t-gray-700 group-hover:border-t-gray-600 transition-colors"></div>
-          </Link>
-        </div>
-      ) : null}
+      {track ? <CurrentTrack open={trackOpen} track={track} /> : null}
     </aside>
   );
 };
