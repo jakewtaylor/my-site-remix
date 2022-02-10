889ed913dcfe3077d6c1176d26e4825089320564
Jake Taylor <me@jaketaylor.co>
2022-02-10 22:39:36 +0000

    add html bg color

--- app/root.tsx
+++ app/root.tsx
@@ -92,7 +92,7 @@ function Document({
   title?: string;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-800">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
