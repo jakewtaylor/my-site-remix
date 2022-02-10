e5c0ccf6fd88a7e67e100ced7ad4581986aa8795
Jake Taylor <me@jaketaylor.co>
2022-02-10 20:16:30 +0000

    Ensure Umami only runs on live

--- app/root.tsx
+++ app/root.tsx
@@ -104,6 +104,7 @@ function Document({
           async
           defer
           data-website-id="f25284cc-bca6-4363-ac55-20107ce03594"
+          data-domains="jaketaylor.co,jaketaylor.dev"
           src="https://umami.jaketaylor.co/umami.js"
         ></script>
       </head>
